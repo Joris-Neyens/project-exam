41829179269c35f6be9cf43f596d89b1d3f8550e
Joris  Neyens <neyens01@gmail.com>
2020-05-14 21:54:49 +0200

    history intro and history
    history intro + style, history js

--- spacex-info/js/spacex.js
+++ spacex-info/js/spacex.js
@@ -50,13 +50,37 @@ state.innerHTML = info.headquarters.state;
 const founded = document.querySelector(".founded");
 founded.innerHTML = info.founded;
 
-const employees = document.querySelector(".employees");
-employees.innerHTML = info.employees;
+const vehicles = document.querySelector(".vehicles");
+vehicles.innerHTML = info.vehicles;
 
 const summary = document.querySelector(".summary");
 summary.innerHTML = info.summary;
 
-
+//company-info
+const companyContainer = document.querySelector(".company-container");
+    companyContainer.innerHTML = `<div class="company-info">
+                <div class="info">
+                    <p>Founder</p>
+                    <p>${info.founder}</p>
+                </div>
+                <div class="info">
+                    <p>CEO</p>
+                    <p>${info.ceo}</p>
+                </div>
+                <div class="info">
+                    <p>Employees</p>
+                    <p>${info.employees}</p>
+                </div>
+                <div class="info">
+                    <p>Valuation</p>
+                    <p>${info.valuation}</p>
+                </div>
+                </div>
+                <div class="company-links">
+                    <a href="${info.links.website}" alt="link to website Spacex">Website</a>
+                    <a href="${info.links.flickr}" alt="link to flickr page of Spacex">flickr</a>
+                    <a href="${info.links.twitter}" alt="link to spacex twitter">Twitter</a>
+                </div>`
 }
 //flight-amounts
 const pastLaunchesUrl = "https://api.spacexdata.com/v3/launches/past";
@@ -81,3 +105,48 @@ function pastLaunches(launches) {
     launchesAmound.innerHTML = launches.length;
     
 }    
+//historic events
+const historyUrl = "https://api.spacexdata.com/v3/history";
+async function getHistory() {
+    try{
+        const response = await fetch(historyUrl);
+        const events = await response.json();
+        history(events);
+    }
+    catch(error) {
+        console.log("error");
+    }
+    finally {
+        console.log("done");
+    }
+}
+getHistory();
+
+function history(events) {
+
+    const historicEvent = document.querySelector(".historic-event");
+    let html = ""
+    
+    events.forEach (function(event) {
+
+        let eventDate = new Date(event.event_date_utc)
+        let formattedDate = eventDate.getFullYear();
+        console.log(formattedDate)
+
+        html += `   <div class="line"></div> 
+                    <h2>${formattedDate}</h2>
+                    <h3>${event.title}</3>
+                    <p>${event.details}</p>
+                    <div class="history-link-container">
+                        <a href="flight.html?id=${event.flight_number}">Flight details</a>
+                        <a href="${event.article}">Article</a>
+                    </div>`
+    });
+
+    historicEvent.innerHTML = html;
+    
+};
+
+
+
+
